3a09815b157bf4196ccdacf9b6509b38a53be849
georg <bumzacktrara@gmail.com>
2018-01-03 23:09:56 +0100

    silenced warnings in main.cpp

--- main.cpp
+++ main.cpp
@@ -7,15 +7,16 @@ using namespace std;
 int main(void) {
   cout << "Hello World" << std::endl;
 
-  Node* a;
-  Node* b; 
-  Node* c;
+  // Node ist eine abstrakte Klasse, davon kann man keine Objekte anlegen :-(
+  // aber man kann zumindest den nullptr zuweisen
+  Node* a = nullptr;
+  Node* b = nullptr; 
+  Node* c = nullptr;
   a->setFirstChild(b);
   b->setNextSibling(c);
   if (a->getFirstChild() == b) {cout << "   b is the first child of a" << endl;}
   if (b->getNextSibling() == c) {cout << "   c is the next sibling of b" << endl;}
   // a.print();
-
 
   return 0;
 }
